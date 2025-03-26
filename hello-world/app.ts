e53423b9fff352e603ee7de66fb8dc54d998ad9c
diegoagtzopt <diego.gutierrez@optimen.com.mx>
2025-03-26 14:36:52 -0600

    feat: add env var

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -15,7 +15,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello world, this is a change from the code',
+                message: 'This is a change with environment variables',
             }),
         };
     } catch (err) {
